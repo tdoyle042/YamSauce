99e2ef4613d8cc44729c979cd97e1b3984e82abb
Salem Hilal <salemhilal@gmail.com>
2013-07-10 16:37:40 -0700

    I DON'T KNOW REQUIRES.JS IM SORRY.

--- yamSauceApp.js
+++
@@ -1,5 +0,0 @@
-//Load All Required Javascript Files
-require(['underscore','backbone','userModel','messageModel','threadModel'],
-	function(underscore,backbone,userModel,messageModel,threadModel){
-	console.log("Got JS Modules for Application");
-});
