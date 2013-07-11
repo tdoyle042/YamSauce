b8eedc1335555ace7e2b6cd5824dd086ad607bbe
Tommy Doyle <tdoyle042@gmail.com>
2013-07-11 11:04:31 -0700

    Deleting parameters that shouldn't be there

--- js/yamSauceApp.js
+++ js/yamSauceApp.js
@@ -1,6 +1,6 @@
 //Load All Required Javascript Files
 require(['underscore','backbone','views/groupView'],
-	function(underscore,backbone,userModel,messageModel,threadModel,GroupView){
+	function(underscore,backbone,GroupView){
 	console.log("Got JS Modules for Application");
 
 	// var testGroup = new GroupView();
