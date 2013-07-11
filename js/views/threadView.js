991e989c24c51537a66355e9194ae3c82db34046
Tommy Doyle <tdoyle042@gmail.com>
2013-07-10 19:25:26 -0700

    Shit

--- js/views/threadView.js
+++ js/views/threadView.js
@@ -1,9 +1,9 @@
-define(['js/backbone','js/models/threadModel'],function(Backbone,Thread){
+define(['js/models/threadModel'],function(Thread){
 	
 	var ThreadView = Backbone.View.extend({
 		className : "threadView",
-		initialize : function(group) {
-			this.group = group;
+		initialize : function(thread) {
+			this.thread = thread;
 		},
 		render : function() {
 			
