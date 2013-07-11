ae1ee99fba73beac5884fb6248850c92fa74df0a
Salem Hilal <salemhilal@gmail.com>
2013-07-10 22:11:55 -0700

    Now everything is there for real this time.

---
+++ js/test.js
@@ -0,0 +1,161 @@
+ var test_data = 
+ {messages: [
+  {
+    from: "Salem Hilal",
+    avatar: "img/salem-avatar.jpg",
+    to: "Yammer Engineering",
+    time: "Jul. 19th, 2:37pm",
+    content: "Put a bird on it blog banjo kitsch, Godard you probably haven't heard of them Marfa hoodie aesthetic selvage wolf meggings sustainable. Etsy keytar vinyl, next level thundercats authentic Carles actually Portland cray craft beer hoodie. Cred DIY Cosby sweater church-key, flannel bicycle rights Carles wayfarers forage. Yr banh mi swag, Bushwick wolf food truck High Life Truffaut hella farm-to-table single-origin coffee. Wolf pickled Schlitz ethical before they sold out whatever, umami gluten-free shoreditch bicycle rights squid Carles.",
+    likes: 12,
+    likes_string: function() {
+      console.log(this.likes)
+      if(this.likes === 1)
+        return "Like";
+      else
+        return "Likes";
+    },
+    comments: [
+      { from: "", time: "", content: "" }, 
+      { from: "", time: "", content: "" }, 
+      { from: "", time: "", content: "" }, 
+      { from: "", time: "", content: "" }, 
+      { from: "", time: "", content: "" }
+    ]
+  },
+  {
+    from: "Salem Hilal",
+    avatar: "img/salem-avatar.jpg",
+    to: "Yammer Engineering",
+    time: "Jul. 19th, 2:37pm",
+    content: "Bespoke Wes Anderson plaid authentic, tote bag meh master cleanse Truffaut helvetica Odd Future pug ugh Pinterest church-key sriracha. PBR dreamcatcher gastropub, asymmetrical four loko umami Etsy raw denim biodiesel tumblr Cosby sweater roof party master cleanse. Etsy keytar vinyl, next level thundercats authentic Carles actually Portland cray craft beer hoodie. Cred DIY banh mi swag, Bushwick wolf food truck High Life Truffaut hella farm-to-table single-origin coffee. Wolf pickled Schlitz ethical before they sold out whatever, umami gluten-free shoreditch bicycle rights squid Carles.",
+    likes: 12,
+    likes_string: function() {
+      if(this.likes === 1)
+        return "Like";
+      else
+        return "Likes";
+    },
+    comments: [
+      { from: "", time: "", content: "" }, 
+      { from: "", time: "", content: "" }, 
+      { from: "", time: "", content: "" }, 
+      { from: "", time: "", content: "" }, 
+      { from: "", time: "", content: "" }
+    ]
+  },
+  {
+    from: "Salem Hilal",
+    avatar: "img/salem-avatar.jpg",
+    to: "Yammer Engineering",
+    time: "Jul. 19th, 2:37pm",
+    content: "Put a bird on it blog banjo kitsch, Godard you probably haven't heard of them Marfa hoodie aesthetic selvage wolf meggings sustainable. Bespoke Wes Anderson plaid authentic, tote bag meh master cleanse Truffaut helvetica Odd Future pug ugh Pinterest church-key sriracha. Etsy keytar vinyl, next level thundercats authentic Carles actually Portland cray craft beer hoodie.",
+    likes: 12,
+    likes_string: function() {
+      if(this.likes === 1)
+        return "Like";
+      else
+        return "Likes";
+    },
+    comments: [
+      { from: "", time: "", content: "" }, 
+      { from: "", time: "", content: "" }, 
+      { from: "", time: "", content: "" }, 
+      { from: "", time: "", content: "" }, 
+      { from: "", time: "", content: "" }
+    ]
+  },
+  {
+    from: "Salem Hilal",
+    avatar: "img/salem-avatar.jpg",
+    to: "Yammer Engineering",
+    time: "Jul. 19th, 2:37pm",
+    content: "Put a bird on it blog banjo kitsch, Godard you probably haven't heard of them Marfa hoodie aesthetic selvage wolf meggings sustainable. Bespoke Wes Anderson plaid authentic, tote bag meh master cleanse Truffaut helvetica Odd Future pug ugh Pinterest church-key sriracha. PBR dreamcatcher gastropub, asymmetrical four loko umami Etsy raw denim biodiesel tumblr Cosby sweater roof party master cleanse. Etsy keytar vinyl, next level thundercats authentic Carles actually Portland cray craft beer hoodie. Cred DIY Cosby sweater church-key, flannel bicycle rights Carles wayfarers forage. Yr banh mi swag, Bushwick wolf food truck High Life Truffaut hella farm-to-table single-origin coffee. Wolf pickled Schlitz ethical before they sold out whatever, umami gluten-free shoreditch bicycle rights squid Carles.",
+    likes: 12,
+    likes_string: function() {
+      if(this.likes === 1)
+        return "Like";
+      else
+        return "Likes";
+    },
+    comments: [
+      { from: "", time: "", content: "" }, 
+      { from: "", time: "", content: "" }, 
+      { from: "", time: "", content: "" }, 
+      { from: "", time: "", content: "" }, 
+      { from: "", time: "", content: "" }
+    ]
+  },
+  {
+    from: "Salem Hilal",
+    avatar: "img/salem-avatar.jpg",
+    to: "Yammer Engineering",
+    time: "Jul. 19th, 2:37pm",
+    content: "Put a bird on it blog banjo kitsch, Godard you probably haven't heard of them Marfa hoodie aesthetic selvage wolf meggings sustainable. Keytar vinyl, next level thundercats authentic Carles actually Portland cray craft beer hoodie. Cred DIY Cosby sweater church-key, flannel bicycle rights Carles wayfarers forage. Yr banh mi swag, Bushwick wolf food truck High Life Truffaut hella farm-to-table single-origin coffee. Wolf pickled Schlitz ethical before they sold out whatever, umami gluten-free shoreditch bicycle rights squid Carles.",
+    likes: 12,
+    likes_string: function() {
+      if(this.likes === 1)
+        return "Like";
+      else
+        return "Likes";
+    },
+    comments: [
+      { from: "", time: "", content: "" }, 
+      { from: "", time: "", content: "" }, 
+      { from: "", time: "", content: "" }, 
+      { from: "", time: "", content: "" }, 
+      { from: "", time: "", content: "" }
+    ]
+  },
+  {
+    from: "Salem Hilal",
+    avatar: "img/salem-avatar.jpg",
+    to: "Yammer Engineering",
+    time: "Jul. 19th, 2:37pm",
+    content: "Put a bird on it blog banjo kitsch, Godard you probably haven't heard of them Marfa hoodie aesthetic selvage wolf meggings sustainable. Bespoke Wes Anderson plaid authentic, tote bag meh master cleanse Truffaut helvetica Odd Future pug ugh Pinterest church-key sriracha. PBR dreamcatcher gastropub, asymmetrical four loko umami Etsy raw denim biodiesel tumblr Cosby sweater roof party master cleanse. Etsy keytar vinyl, next level thundercats authentic Carles actually Portland cray craft beer hoodie.",
+    likes: 12,
+    likes_string: function() {
+      if(this.likes === 1)
+        return "Like";
+      else
+        return "Likes";
+    },
+    comments: [
+      { from: "", time: "", content: "" }, 
+      { from: "", time: "", content: "" }, 
+      { from: "", time: "", content: "" }, 
+      { from: "", time: "", content: "" }, 
+      { from: "", time: "", content: "" }
+    ]
+  }
+]}
+
+var message_template = 
+"{{#messages}}" +
+"<div class='message-wrapper'>" +
+  "<div class='message'>" +
+
+    // Header
+    "<header>" +
+      "<div class='header-row'>" +
+        "<div class='avatar'>" +
+          "<img src='{{{avatar}}}' />" +
+        "</div>" +
+        "<div class='to-from'>" +
+          "{{from}}<br><span>to {{to}}</span>" +
+        "</div>" +
+      "</div>  " +
+      "<div class='timestamp'>{{timestamp}}</div>" +
+    "</header>" +
+
+    // Body
+    "<div class='content'>" +
+      "{{content}} " +
+    "</div>" +
+
+    // Footer
+    "<footer>" +
+      "<div class='meta'>{{likes}} Likes - 5 Comments</div>" +
+    "</footer>" +
+  "</div>" +
+"</div>" +
+"{{/messages}}";
