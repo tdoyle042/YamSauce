7c24327f2c091f86e54d2e7941f62901013d5fc4
Matt Schallert <mattschallert@gmail.com>
2013-07-12 01:18:52 -0700

    Use higher quality mugshots

--- public/js/models/userModel.js
+++ public/js/models/userModel.js
@@ -2,6 +2,9 @@ var UserModel = Backbone.Model.extend({
 	initialize : function (userData) {
 		this.id = userData.id;
 		this.full_name = userData.full_name;
-		this.mugshot_url = userData.mugshot_url;
+
+    var mug_template = userData.mugshot_url_template;
+    var better_mug_url = mug_template.replace("{width}x{height}", "75x75");
+		this.mugshot_url = better_mug_url;
 	}
 });
