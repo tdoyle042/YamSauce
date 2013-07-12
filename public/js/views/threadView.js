019ffea6639294d6c9870d9555184a14874e48da
Salem Hilal <salemhilal@gmail.com>
2013-07-11 23:48:41 -0700

    Messages look better.

--- public/js/views/threadView.js
+++ public/js/views/threadView.js
@@ -17,26 +17,46 @@ var ThreadView = Backbone.View.extend({
 		var el = Mustache.render(this.template.html(),this.data);
 		elem.append(el);
 
+		// WHEN YOU CLOSE THINGS THEY CLOSE!
+		$(".close").click(function(){
+		  // Super lame, I know, but hackathon so whatever
+		  console.log("shadowbox click!");
+		  $(".shadowbox").click();
+		});
+
+		// WHEN YOU CLICK MESSAGES THEY COME INTO FOCUS HELL YEAH.
 		$(".message").click(function() {
 		  if($(this).parent().hasClass("active"))
 		    return;
 
 		  var shadow = $("<div />").addClass("shadowbox").hide().click(function(){
-		    $(".active").removeClass("active pure-u-1").addClass("pure-u-1-2")
-		        .find(".message")
-		          .removeClass("pure-u-1-2")
-		        .end().find(".meta-comments")
-		          .show()
-		        .end()
-		        .find(".close")
-		          .hide()
-		        .end()
-		        .height("auto");
+		    var that = $(".active")
 
-		    $(this).fadeOut(function(){
-		      $(".shadowbox").remove();
-		    });
-		    $("content").masonry();
+		    that
+		    		.find(".close").hide().end()
+		    		.find(".comments")
+		    			.animate({marginLeft: "-50%"}, 300, function(){
+		    				that
+		    						.removeClass("active pure-u-1").addClass("pure-u-1-2")
+		    				    .find(".message")
+		    				      .removeClass("pure-u-1-2")
+		    				    .end().find(".meta-comments")
+		    				      .show()
+		    				    .end()
+		    				    .find(".close")
+		    				      .hide()
+		    				    .end()
+		    				    .height("auto");
+		    				    // .animate({width: "50%"});
+
+		    				    $(this).fadeOut(function(){
+		    				      $(".shadowbox").remove();
+		    				    });
+		    				    $("content").masonry();
+		    			})
+		    		
+
+		    
 		  });
 
 		  $("body").append(shadow);
@@ -44,14 +64,28 @@ var ThreadView = Backbone.View.extend({
 
 		  var current_height = $(this).parent().height();
 
-		  $(this).parent()
-		    .addClass("active pure-u-1")
+		  var that = $(this).parent();
+		  that.height(Math.max(current_height, 550));
+		  that.find(".meta-comments").hide();
+
+		  that
+		    .addClass("active")
 		    .removeClass("pure-u-1-2")
-		    .find(".close").show().end()
-		    .height(Math.max(current_height, 550))
-		    .siblings().removeClass("active");
-		  $(this).addClass("pure-u-1-2");
-		  $(this).find(".meta-comments").hide();
+		    .find(".comments").show()
+		    .animate({marginLeft: "0%"}, 400, function(){
+			    that
+			      .find(".close").show().end()
+			      .siblings().removeClass("active");
+			    $(this).addClass("pure-u-1-2");
+		    });
+		  
 		});
+
+		$(".group-threads").masonry({
+		  columnWidth: '.message-wrapper',
+		  itemSelector: '.message-wrapper'
+		});
+
+
 	}
 });
--- public/js/yamSauceApp.js
+++ public/js/yamSauceApp.js
@@ -21,7 +21,7 @@ $(document).ready(function() {
   //
   // GET MASONRY GOIN'
   //
-  $(".content").masonry({
+  $(".group-threads").masonry({
     columnWidth: '.message-wrapper',
     itemSelector: '.message-wrapper'
   });
@@ -29,51 +29,6 @@ $(document).ready(function() {
   //
   // EVENT LISTENERS
   // 
-
-  // WHEN YOU CLICK THINNGS THEY GET FOCUSED. HOT DOG!
-  $(".message").click(function() {
-    if($(this).parent().hasClass("active"))
-      return;
-
-    var shadow = $("<div />").addClass("shadowbox").hide().click(function(){
-      $(".active").removeClass("active pure-u-1").addClass("pure-u-1-2")
-          .find(".message")
-            .removeClass("pure-u-1-2")
-          .end().find(".meta-comments")
-            .show()
-          .end()
-          .find(".close")
-            .hide()
-          .end()
-          .height("auto");
-
-      $(this).fadeOut(function(){
-        $(".shadowbox").remove();
-      });
-      $("content").masonry();
-    });
-
-    $("body").append(shadow);
-    $(".shadowbox").fadeIn();
-
-    var current_height = $(this).parent().height();
-
-    $(this).parent()
-      .addClass("active pure-u-1")
-      .removeClass("pure-u-1-2")
-      .find(".close").show().end()
-      .height(Math.max(current_height, 550))
-      .siblings().removeClass("active");
-    $(this).addClass("pure-u-1-2");
-    $(this).find(".meta-comments").hide();
-  });
-
-  // WHEN YOU CLOSE THINGS THEY CLOSE!
-  $(".close").click(function(){
-    // Super lame, I know, but hackathon so whatever
-    console.log("shadowbox click!");
-    $(".shadowbox").click();
-  });
 
   // WHEN YOU HOVER OVER THE YAMMER THING IT BRINGS DA MENU IN.
   $(".sidebar").css("margin-left","-350px");
