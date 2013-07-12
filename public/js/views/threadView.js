4e3d5265aa27cbf097def4396cf371c7f6c68210
Tommy Doyle <tdoyle042@gmail.com>
2013-07-11 20:36:18 -0700

    Connecting more things to views and such

--- public/js/views/threadView.js
+++ public/js/views/threadView.js
@@ -2,8 +2,18 @@ var ThreadView = Backbone.View.extend({
 	className : "threadView",
 	initialize : function(params) {
 		this.thread = params.thread;
-		this.template = $("")
+		this.template = $("#thread-template");
 	},
-	render : function() {
+	render : function(elem) {
+		var threadStart = this.thread.threadStarter;
+		this.data = {
+			avatar : "",
+			from : threadStarter.poster_id,
+			to ; "Bob",
+			"time-stamp" : threadStarter.timestamp,
+			content : threadStarter.text,
+		};
+		var el = Mustache.render(this.template,this.data);
+		elem.append(el);
 	}
 });
