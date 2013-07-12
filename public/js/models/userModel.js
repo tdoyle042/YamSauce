4cd5e9463cb7b579d1ee1fe4acdc2d39fb44f99f
Salem Hilal <salemhilal@gmail.com>
2013-07-12 01:30:49 -0700

    Retina mugshots.

--- public/js/models/userModel.js
+++ public/js/models/userModel.js
@@ -4,7 +4,7 @@ var UserModel = Backbone.Model.extend({
 		this.full_name = userData.full_name;
 
     var mug_template = userData.mugshot_url_template;
-    var better_mug_url = mug_template.replace("{width}x{height}", "75x75");
+    var better_mug_url = mug_template.replace("{width}x{height}", "150x150");
 		this.mugshot_url = better_mug_url;
 	}
 });
