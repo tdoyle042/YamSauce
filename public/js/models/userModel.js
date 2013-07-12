ea81f18f3929f00e1bea7b10b2f2116d6da5e724
Matt Schallert <mattschallert@gmail.com>
2013-07-12 01:08:07 -0700

    Change name property on user
    Make it match up with the API 'cause i'm OCD as fuck

--- public/js/models/userModel.js
+++ public/js/models/userModel.js
@@ -1,7 +1,7 @@
 var UserModel = Backbone.Model.extend({
 	initialize : function (userData) {
 		this.id = userData.id;
-		this.fullname = userData.full_name;
+		this.full_name = userData.full_name;
 		this.mugshot_url = userData.mugshot_url;
 	}
 });
