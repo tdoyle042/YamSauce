6b5f69d03a22c106ba45c67d568ad67a35661d57
Matt Schallert <mattschallert@gmail.com>
2013-07-29 12:56:19 -0700

    Require ssl in production

--- app.rb
+++ app.rb
@@ -1,6 +1,20 @@
 require 'rubygems'
 require 'sinatra'
 
+configure :production do
+
+  # us a simple before filter to redirect all requests in production to https
+  # before do
+  #     unless (@env['HTTP_X_FORWARDED_PROTO'] || @env['rack.url_scheme'])=='https'
+  #       redirect "https://#{request.env['HTTP_HOST']}#{request.env["REQUEST_PATH"]}"
+  #     end
+  #   end
+
+ #even simpler use rack-ssl-enforcer rack middle ware to do the same thing
+  require 'rack-ssl-enforcer'
+  use Rack::SslEnforcer
+end
+
 get '/' do
   File.read(File.join('public', 'home.html'))
 end
